91e2a3d60473768f387473b5f1a3255d762af15c
lovely-necromancer <lovely_necromancer@proton.me>
2025-11-29 00:39:35 +0000

    update vimz

---
+++ vimz/_next/static/chunks/app/page-82b4869606c1aa37.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{5233:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,2619,23)),Promise.resolve().then(n.t.bind(n,1356,23))}},e=>{e.O(0,[850,441,255,358],()=>e(e.s=5233)),_N_E=e.O()}]);
